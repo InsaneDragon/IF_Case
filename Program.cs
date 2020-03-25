32cd92bdd4158547ef55d8aff81cad2794dee91a
C#Developer <shahzodkiller11@gmail.com>
2020-03-25 13:27:03 +0500

    If

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace IF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2
+            // double a=6,b=6;
+            // if(b==a)a=0;b=0;
+            // if(a>b)b=a;
+            // if(b>a)a=b;
+            // System.Console.WriteLine("a:"+a);
+            // System.Console.WriteLine("b:"+b);
+
+            //3
+            // double operand1,operand2;
+            // string sign;
+            // Console.Write("Write first number:");
+            // operand1 = double.Parse(Console.ReadLine());
+            // Console.Write("Write second number:");
+            // operand2 = double.Parse(Console.ReadLine());
+            // Console.WriteLine("Choose an operation:");
+            // sign = Console.ReadLine();
+            // switch (sign)
+            // {
+            //     case  "+":
+            //     Console.WriteLine(operand1 + operand2);
+            //     break;
+            //      case  "-":
+            //     Console.WriteLine(operand1 - operand2);
+            //     break;
+            //      case  "*":
+            //     Console.WriteLine(operand1 * operand2);
+            //     break;
+            //      case  "/":
+            //      if (operand1 ==0)
+            //      {
+            //          Console.WriteLine("Ошибка");
+            //          break;
+            //      }
+            //      if(operand2 ==0)
+            //      {
+            //          System.Console.WriteLine("Ошибка");
+            //      }
+            //      else 
+            //      {
+            //     Console.WriteLine(operand1 / operand2);
+                    
+            //      }break;
+            //     default:
+            //     Console.WriteLine("Произошла Ошибка!!!");
+            //     break;
+            // }
+
+            //4
+            // int num=99;
+            // if(num>=0&&num<=14)
+            // {System.Console.WriteLine("0<14");}
+            // else if(num>=15&&num<=35)
+            // {System.Console.WriteLine("15<35");}
+            // else if(num>=36&&num<=50)
+            // {System.Console.WriteLine("36<50");}
+            // else if(num>=50&&num<=100)
+            // {System.Console.WriteLine("50<100");}
+            // else 
+            // {System.Console.WriteLine("this number is not included in any interval");}
+        }
+    }
+}
